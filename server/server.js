feff71a80e13d5acc21e2d6d67f23694beb70b55
shahlahassan123 <ahlan.shahla@gmail.com>
2024-02-22 18:50:47 -0600

    first commit

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 // app.use(cors())
 app.use(cors(
     {
-        origin : ["https://ecommerce-mern-sooty.vercel.app"],
+        origin : ["https://ecommerce-mern-n5ro.vercel.app"],
         methods : ["GET", "POST", "PUT", "DELETE"] ,
         credentials: true
     }

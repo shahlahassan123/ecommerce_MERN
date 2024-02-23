9329f852bc4dc440e79c32edb729db279f4a4e1d
shahlahassan123 <ahlan.shahla@gmail.com>
2024-02-22 18:49:32 -0600

    first commit

--- server/server.js
+++ server/server.js
@@ -10,7 +10,15 @@ dotenv.config();
 const app = express();
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+app.use(cors(
+    {
+        origin : ["https://ecommerce-mern-sooty.vercel.app"],
+        methods : ["GET", "POST", "PUT", "DELETE"] ,
+        credentials: true
+    }
+))
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use("/", routes)
